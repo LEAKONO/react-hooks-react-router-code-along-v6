027f809557de9b595d01718b872bd7e418ea8356
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-05-01 16:11:25 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
